be289953dc26b0e9472bd0914438e082b4d59a33
StanceFreak <jombor.army67@gmail.com>
2022-06-11 02:37:17 +0800

    Adding favorite and the detail page

--- app/src/main/java/com/example/subimisidicoding/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/example/subimisidicoding/ui/activity/DetailActivity.kt
@@ -3,7 +3,9 @@ package com.example.subimisidicoding.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.subimisidicoding.R
+import com.example.subimisidicoding.data.Film
 import com.example.subimisidicoding.databinding.ActivityDetailBinding
+import com.squareup.picasso.Picasso
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,16 +15,43 @@ class DetailActivity : AppCompatActivity() {
         const val FILM_EXTRAS = "film_extras"
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
 
         setupUI()
+        setupActionBar()
 
         setContentView(binding.root)
     }
 
+    private fun setupActionBar() {
+        val actionBar = supportActionBar
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            elevation = 0f
+        }
+    }
+
     private fun setupUI() {
-        TODO("Not yet implemented")
+        val data = intent.getParcelableExtra<Film>(FILM_EXTRAS)
+
+        if (data != null) {
+            Picasso.get().load(data.poster).into(binding.ivDetailPoster)
+            binding.apply {
+                tvDetailTitle.text = data.title
+                tvDetailDirector.text = "Sutradara : ${data.sutaradara}"
+                tvDetailCast.text = "Pemeran : ${data.pemeran}"
+                tvDetailRelease.text = "Tanggal Rilis : ${data.tanggalRilis}"
+                tvDetailGenre.text = "Genre : ${data.genre}"
+                tvDetailDesc.text = data.desc
+            }
+        }
     }
 }
--- app/src/main/java/com/example/subimisidicoding/ui/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/subimisidicoding/ui/fragment/FavoriteFragment.kt
@@ -1,19 +1,65 @@
 package com.example.subimisidicoding.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.subimisidicoding.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.subimisidicoding.databinding.FragmentFavoriteBinding
+import com.example.subimisidicoding.local.FilmFavEntity
+import com.example.subimisidicoding.ui.adapter.AdapterFavorite
+import com.example.subimisidicoding.viewmodel.FilmFavViewModel
 
 class FavoriteFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoriteBinding
+    private lateinit var favAdapter: AdapterFavorite
+    private lateinit var viewModel: FilmFavViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecycler()
+    }
+
+    private fun setupRecycler() {
+        favAdapter = AdapterFavorite()
+        binding.rvFavorite.apply {
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            adapter = favAdapter
+            setHasFixedSize(true)
+        }
+
+        viewModel = ViewModelProvider(this).get(FilmFavViewModel::class.java)
+        Log.d("test", viewModel.getFavorite.toString())
+
+        if (viewModel.getFavorite.value != null) {
+            binding.layoutError.visibility = View.GONE
+            binding.rvFavorite.visibility = View.VISIBLE
+            viewModel.getFavorite.observe(viewLifecycleOwner, Observer {
+                favAdapter.setFavorite(it as ArrayList<FilmFavEntity>)
+            })
+        }
+        else {
+            binding.layoutError.visibility = View.VISIBLE
+            binding.rvFavorite.visibility = View.GONE
+        }
     }
 
 }
