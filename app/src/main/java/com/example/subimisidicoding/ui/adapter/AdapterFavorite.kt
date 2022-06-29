048675de389ce72f4049d9747ecae47b8a7a4fdf
StanceFreak <jombor.army67@gmail.com>
2022-06-29 18:12:33 +0800

    Added intent to movie detail

--- app/src/main/java/com/example/subimisidicoding/ui/adapter/AdapterFavorite.kt
+++ app/src/main/java/com/example/subimisidicoding/ui/adapter/AdapterFavorite.kt
@@ -1,10 +1,12 @@
 package com.example.subimisidicoding.ui.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.subimisidicoding.databinding.RecyclerMainBinding
 import com.example.subimisidicoding.local.FilmFavEntity
+import com.example.subimisidicoding.ui.activity.DetailActivity
 import com.squareup.picasso.Picasso
 
 class AdapterFavorite() : RecyclerView.Adapter<AdapterFavorite.FavViewHolder>() {
@@ -32,6 +34,12 @@ class AdapterFavorite() : RecyclerView.Adapter<AdapterFavorite.FavViewHolder>() 
     override fun onBindViewHolder(holder: FavViewHolder, position: Int) {
         val dataList = this.data[position]
         holder.bind(dataList)
+
+        holder.itemView.setOnClickListener {
+            val i = Intent(holder.itemView.context, DetailActivity::class.java)
+            i.putExtra(DetailActivity.FILM_EXTRAS, dataList.movieId)
+            holder.itemView.context.startActivity(i)
+        }
     }
 
     override fun getItemCount(): Int {
